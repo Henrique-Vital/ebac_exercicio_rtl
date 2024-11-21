91d70b817c5e91c3cbbcebd622470017bd6d58cd
Henrique Vital <hbvbrasil@gmail.com>
2024-11-21 16:30:33 -0300

    Correção data-testid
    Correção data-testid

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,29 +1,25 @@
-import { fireEvent, render, screen } from '@testing-library/react'
-import Post from '.';
+import { fireEvent, render, screen } from '@testing-library/react';
 import PostComment from '.';
-import userEvent from '@testing-library/user-event';
-
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
-    test('inserir dois comentários', () => {
-        render(<Post />);
-        
-        const textarea = screen.getByTestId('post-comments-form-textarea');
-        const button = screen.getByTestId('post-comments-form-button');
-        userEvent.type(textarea, 'Este é o primeiro comentário');
-        fireEvent.click(button);
-    
-        const primeiroComentario = screen.getByText('Este é o primeiro comentário');
-        expect(primeiroComentario).toBeInTheDocument();
-    
-        userEvent.type(textarea, 'Este é o segundo comentário');
-        fireEvent.click(button);
-    
-        const segundoComentario = screen.getByText('Este é o segundo comentário');
-        expect(segundoComentario).toBeInTheDocument();
-      });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment />);
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: { value: 'Primeiro Comentario' },
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: { value: 'Segundo Comentario' },
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
     });
+});
